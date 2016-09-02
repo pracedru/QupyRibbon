1f92c5db4f36954e23171f7d321f0200efe412ff
magnus <magnusmj@gmail.com>
2016-09-02 13:37:22 +0200

    more cleanup.

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QKeySequence
+from PyQt5.QtGui import QKeySequence as QKSec
 from GUI.RibbonButton import RibbonButton
 from GUI.Icons import get_icon
 from GUI.RibbonWidget import *
@@ -22,67 +22,32 @@ class MainWindow(QMainWindow):
 
         # -------------      actions       -----------------
 
-        self._open_action = QAction(get_icon("open"), "Open\nFile...", self)
-        self._open_action.setShortcuts(QKeySequence.Open)
-        self._open_action.setStatusTip("Open file")
-        self._open_action.triggered.connect(self.on_open_file)
-        self._open_action.setIconVisibleInMenu(True)
-        self.addAction(self._open_action)
-
-        # _save_action
-        self._save_action = QAction(get_icon("save"), "Save", self)
-        self._save_action.setShortcuts(QKeySequence.Save)
-        self._save_action.setStatusTip("Save these data")
-        self._save_action.triggered.connect(self.on_save)
-        self._save_action.setIconVisibleInMenu(True)
-        self.addAction(self._save_action)
-
-        self._copy_action = QAction(get_icon("copy"), "Copy", self)
-        self._copy_action.setShortcuts(QKeySequence.Copy)
-        self._copy_action.setStatusTip("Copy selection")
-        self._copy_action.triggered.connect(self.on_copy)
-        self._copy_action.setIconVisibleInMenu(True)
-        self.addAction(self._copy_action)
-
-        self._paste_action = QAction(get_icon("paste"), "Paste", self)
-        self._paste_action.setShortcuts(QKeySequence.Copy)
-        self._paste_action.setStatusTip("Paste from clipboard")
-        self._paste_action.triggered.connect(self.on_paste)
-        self._paste_action.setIconVisibleInMenu(True)
-        self.addAction(self._paste_action)
-
-        self._zoom_action = QAction(get_icon("zoom"), "Zoom", self)
-        self._zoom_action.setStatusTip("Zoom in on document")
-        self._zoom_action.triggered.connect(self.on_zoom)
-        self._zoom_action.setIconVisibleInMenu(True)
-        self.addAction(self._zoom_action)
-
-        self._about_action = QAction(get_icon("about"), "About", self)
-        self._about_action.setStatusTip("About QupyRibbon")
-        self._about_action.triggered.connect(self.on_about)
-        self._about_action.setIconVisibleInMenu(True)
-        self.addAction(self._about_action)
-
-        self._license_action = QAction(get_icon("license"), "License", self)
-        self._license_action.setStatusTip("Licence for this software")
-        self._license_action.triggered.connect(self.on_license)
-        self._license_action.setIconVisibleInMenu(True)
-        self.addAction(self._license_action)
+        self._open_action = self.add_action("Open", "open", "Open file", True, self.on_open_file, QKSec.Open)
+        self._save_action = self.add_action("Save", "save", "Save file", True, self.on_save, QKSec.Save)
+        self._copy_action = self.add_action("Copy", "copy", "Copy selection", True, self.on_copy, QKSec.Copy)
+        self._paste_action = self.add_action("Paste", "paste", "Paste from clipboard", True, self.on_paste, QKSec.Paste)
+        self._zoom_action = self.add_action("Zoom", "zoom", "Zoom in on document", True, self.on_zoom)
+        self._about_action = self.add_action("About", "about", "About QupyRibbon", True, self.on_about)
+        self._license_action = self.add_action("License", "license", "Licence for this software", True, self.on_license)
 
         # Ribbon
 
-        self._ribbon = QToolBar(self)
-        self._ribbon.setStyleSheet(get_stylesheet("ribbon"))
-        self._ribbon.setObjectName("ribbonWidget")
-        self._ribbon.setWindowTitle("Ribbon")
+        self._ribbon = RibbonWidget(self)
         self.addToolBar(self._ribbon)
-        self._ribbon.setMovable(False)
-        self._ribbon_widget = RibbonWidget(self)
-        self._ribbon.addWidget(self._ribbon_widget)
         self.init_ribbon()
 
+    def add_action(self, caption, icon_name, status_tip, icon_visible, connection, shortcut=None):
+        action = QAction(get_icon(icon_name), caption, self)
+        action.setStatusTip(status_tip)
+        action.triggered.connect(connection)
+        action.setIconVisibleInMenu(icon_visible)
+        if shortcut is not None:
+            action.setShortcuts(shortcut)
+        self.addAction(action)
+        return action
+
     def init_ribbon(self):
-        home_tab = self._ribbon_widget.add_ribbon_tab("Home")
+        home_tab = self._ribbon.add_ribbon_tab("Home")
         file_pane = home_tab.add_ribbon_pane("File")
         file_pane.add_ribbon_widget(RibbonButton(self, self._open_action, True))
         file_pane.add_ribbon_widget(RibbonButton(self, self._save_action, True))
@@ -114,7 +79,7 @@ class MainWindow(QMainWindow):
         view_panel.add_ribbon_widget(RibbonButton(self, self._zoom_action, True))
         home_tab.add_spacer()
 
-        about_tab = self._ribbon_widget.add_ribbon_tab("About")
+        about_tab = self._ribbon.add_ribbon_tab("About")
         info_panel = about_tab.add_ribbon_pane("Info")
         info_panel.add_ribbon_widget(RibbonButton(self, self._about_action, True))
         info_panel.add_ribbon_widget(RibbonButton(self, self._license_action, True))
--- GUI/RibbonWidget.py
+++ GUI/RibbonWidget.py
@@ -1,17 +1,24 @@
 from PyQt5.QtWidgets import *
 from GUI.RibbonTab import RibbonTab
 from GUI import gui_scale
+from GUI.StyleSheets import get_stylesheet
 
 __author__ = 'magnus'
 
 
-class RibbonWidget(QTabWidget):
-    def __init__(self, main_window):
-        QWidget.__init__(self, main_window)
-        self.setMaximumHeight(120*gui_scale())
-        self.setMinimumHeight(110*gui_scale())
+class RibbonWidget(QToolBar):
+    def __init__(self, parent):
+        QToolBar.__init__(self, parent)
+        self.setStyleSheet(get_stylesheet("ribbon"))
+        self.setObjectName("ribbonWidget")
+        self.setWindowTitle("Ribbon")
+        self._ribbon_widget = QTabWidget(self)
+        self._ribbon_widget.setMaximumHeight(120*gui_scale())
+        self._ribbon_widget.setMinimumHeight(110*gui_scale())
+        self.setMovable(False)
+        self.addWidget(self._ribbon_widget)
 
     def add_ribbon_tab(self, name):
         ribbon_tab = RibbonTab(self, name)
-        self.addTab(ribbon_tab, name)
+        self._ribbon_widget.addTab(ribbon_tab, name)
         return ribbon_tab
